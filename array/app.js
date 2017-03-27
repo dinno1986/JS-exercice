9c0009fb20fa2904296fc5ffcc0850eb0110339a
dinno1986 <dimitri.castell@hotmail.fr>
2017-03-27 11:41:00 +0200

    modif du mois d'août

--- array/app.js
+++ array/app.js
@@ -9,3 +9,9 @@ console.log(mois[4]);
 
 //afficher le mois de Novembre
 console.log(mois[10]);
+
+// modifier le mois d'Aout
+mois.splice(8,0,"Août");
+console.log(mois);
+mois.splice(7,1);
+console.log(mois);
