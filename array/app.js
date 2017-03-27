993dcc2edc2f5acd608cd29420a6cd549d794d2f
dinno1986 <dimitri.castell@hotmail.fr>
2017-03-27 10:59:19 +0200

    afficher index 5 du tableau

--- array/app.js
+++ array/app.js
@@ -1,4 +1,8 @@
 var mois = ['Janvier','Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'];
 console.log(mois);
+
 //aficher 3eme valeur
 console.log(mois[2]);
+
+//afficher l'index 5
+console.log(mois[4]);
